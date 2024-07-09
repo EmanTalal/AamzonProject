bd80fbb03682a1cff5534e1d7e8ba63ec13e436f
github_EmanTalal <emantalal.201999@gmail.com>
2024-07-09 19:13:02 +0300

    emn

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+
+function Login() {
+    const[username,setUsername]=useState('')
+    const [password,setPassword]=useState('')
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
